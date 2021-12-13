2c517eecc47f24f94032ee4c1d77252f14e66876
ossamaabdein <ossamaabdein@gmail.com>
2021-12-13 14:21:33 +0200

    demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -8,6 +9,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { SignupComponent } from './components/signup/signup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  tasksList:any = [];
+  completedTasks: any = [];
+  activeTasks: any = [];
   constructor() { }
 
   ngOnInit(): void {
+    if (localStorage.getItem('list') !== null) {
+      this.tasksList = JSON.parse(localStorage.getItem('list')!);
+    }
+  }
+
+
+  inputField:any = document.getElementsByClassName('taskTitle');
+  addTask(userInput:any) {
+    if (userInput.value !== "") {
+      this.tasksList.push(userInput.value);
+      userInput.value = '';
+      localStorage.setItem('list', JSON.stringify(this.tasksList));
+    }
+  }
+
+  deleteTask(i:number) {
+    this.tasksList.splice(i,1);
+    localStorage.setItem('list', JSON.stringify(this.tasksList));
+  }
+
+  decorate(i:any) {
+    if(!this.inputField[i].classList.contains('checked')) {
+      this.inputField[i].classList.add('checked');
+      this.completedTasks.push(this.inputField[i].innerText);
+      localStorage.setItem('completed', this.completedTasks);
+      this.activeTasks.splice(i,1);
+      this.activeTasks = this.tasksList.filter((x:any) => !this.completedTasks.includes(x));
+    } else {
+      this.inputField[i].classList.remove('checked');
+      this.completedTasks.splice(i,1);
+      localStorage.setItem('completed', this.completedTasks);
+      this.activeTasks.push(this.inputField[i].innerText);
+      this.activeTasks = this.tasksList.filter((x:any) => !this.completedTasks.includes(x));
+    }
+  }
+
+  displayAll() {
+    this.tasksList = JSON.parse(localStorage.getItem('list')!);
+  }
+
+  displayActive() {
+    // console.log(this.tasksList)
+    // console.log(this.completedTasks)
+    // this.activeTasks = this.tasksList.filter((x:any) => !this.completedTasks.includes(x));
+    console.log(this.activeTasks);
+    // this.tasksList = this.activeTasks;
+  }
+
+  displayCompleted() {
+    this.tasksList = this.completedTasks;
+  }
+
+  clearCompleted() {
+    this.tasksList = this.tasksList.filter((x:any) => !this.completedTasks.includes(x));
   }
 
 }
